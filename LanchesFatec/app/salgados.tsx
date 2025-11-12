03bdbd3966c181b13bc005d1aeef745805c02b67
Lizandraferrari <lizandra.ferrari@hotmail.com>
2025-11-11 23:14:32 -0300

    Implementação do get filtrado pela API

--- LanchesFatec/app/salgados.tsx
+++ LanchesFatec/app/salgados.tsx
@@ -1,23 +1,37 @@
 import { View, FlatList, StyleSheet } from 'react-native';
 import ItemCard from '../components/ItemCard';
 import CategoriesBase from '../components/CategoriesBase';
+import api from '@/utils/api';
+import { useEffect, useState } from 'react';
+
+interface Produto {
+  _id: string;
+  nome: string;
+  preco: number;
+  imagemUrl: string;
+}
 
 export default function Salgados() {
-  const image = 'https://emporiokaminski.com.br/wp-content/uploads/2024/06/Mini-Empada-Frango-1.jpg'
+  const [item , setItem] = useState<Produto[]>([]);
 
-  const itens = [
-    { id: '1', nome: 'Lanche de pernil com queijo' , preco: 6.00 , image: image },
-    { id: '2', nome: 'Bebida' , preco: 4.50 , image: image },
-    { id: '3', nome: 'Bomboniere' , preco: 3.75 , image: image },
-    { id: '4', nome: 'Sobremesa' , preco: 5.00 , image: image },
-  ]
-  
+  async function fetchItems() {
+    try {
+      const response = await api.get('/api/produtos?categoria=Lanche');
+      setItem(response.data);
+    } catch (error) {
+      console.error('Error fetching items:', error);
+    }
+  }
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
   return (
     <CategoriesBase title='Lanches' subtitle='Menu de Lanches'>
       <FlatList
-        data={itens}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) =><ItemCard id={item.id} image={item.image} nome={item.nome} preco={item.preco} />}
+        data={item}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) =><ItemCard id={item._id} image={item.imagemUrl} nome={item.nome} preco={item.preco} />}
         showsVerticalScrollIndicator={false}
       />
     </CategoriesBase>
--- LanchesFatec/utils/api.ts
+++ LanchesFatec/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://fateclanchesback.onrender.com',
+  baseURL: /*'https://fateclanchesback.onrender.com',*/'http://192.168.18.8:3000',
   headers: {
     'Content-Type': 'application/json',
   },
