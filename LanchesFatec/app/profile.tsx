47c7cc8fe4f94d331fe01257a56013d70cae4633
Lizandraferrari <lizandra.ferrari@hotmail.com>
2025-11-15 18:12:05 -0300

    Funcionalidades do perfil

--- LanchesFatec/app/profile.tsx
+++ LanchesFatec/app/profile.tsx
@@ -1,20 +1,78 @@
-import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, TouchableOpacity, View, Button, Alert, Linking } from "react-native";
 import BasePage from "@/components/BasePage";
 import Btn from "@/components/Btn";
 import TextFont from '@/components/TextFont';
+import * as ImagePicker from 'expo-image-picker';
+import { useEffect, useState } from "react";
+import * as SecureStore from 'expo-secure-store';
+import { useRouter } from "expo-router";
 
 export default function Profile() {
+    const router = useRouter()
+
     const user = {
-        nome: 'João Silva',
+        nome: 'Fulano da Silva Souza',
         email: 'fulano.silva@fatec.sp.gov.br',
         fatec: 'Fatec Mauá'
+    };
+    function issues() {
+        Alert.alert('Aviso!', 'Você será redirecionado para nosso suporte', [
+            {
+                text: "Ficar no Fatec Lanches",
+                style: "cancel"
+            },
+            {
+                text: "Ok",
+                onPress: () => Linking.openURL('https://github.com/Lizandraferrari/fatec-lanches/issues')
+            }
+        ])
+
     }
+    function confirm() {
+        Alert.alert('Você quer mesmo sair?', '', [
+            {
+                text: "Não",
+                style: "cancel"
+            },
+            {
+                text: "Sim",
+                onPress: () => handleLogout()
+            }
+        ])
+        async function handleLogout() {
+            await SecureStore.deleteItemAsync('token');
+            router.push('/login');
+        }
+    }
+
+    const [profileImage, setProfileImage] = useState<string | null>(null);
+
+    const pickImage = async () => {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+            alert('Permissão negada!');
+            return;
+        }
+
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setProfileImage(result.assets[0].uri);
+        }
+    };
 
     return (
         <BasePage title="Perfil" subtitle="Gerencie suas informações">
             <View style={styles.card}>
                 <Image
-                    source={require('@/assets/empada.jpeg')}
+                    source={
+                        profileImage
+                            ? { uri: profileImage }
+                            : require('@/assets/empada.jpeg')
+                    }
                     style={styles.image}
                 />
 
@@ -22,13 +80,14 @@ export default function Profile() {
                 <TextFont style={styles.email}>{user.email}</TextFont>
                 <TextFont style={styles.fatec}>{user.fatec}</TextFont>
             </View>
+
             <View style={styles.options}>
-                <Btn variant="cyan">Editar Perfil</Btn>
-                <Btn variant="red">Sair</Btn>
+                <Btn variant="cyan" onPress={pickImage}>Editar Perfil</Btn>
+                <Btn variant="red" onPress={confirm}>Sair</Btn>
                 <TouchableOpacity>
                     <TextFont style={styles.link}>Excluir Conta</TextFont>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={issues}>
                     <TextFont style={styles.link}>Relatar um Problema</TextFont>
                 </TouchableOpacity>
             </View>
