9093f93084fa3265520e857bac5bab12f137ba29
Lizandraferrari <lizandra.ferrari@hotmail.com>
2025-11-27 18:50:43 -0300

    Ajustes para o APK

--- LanchesFatec/app/_layout.tsx
+++ LanchesFatec/app/_layout.tsx
@@ -1,31 +1,21 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { View, StyleSheet } from 'react-native';
-import { useFonts, Roboto_300Light , Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-
-SplashScreen.preventAutoHideAsync();
+import { useFonts, Roboto_300Light, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
-    Roboto_300Light ,
+    Roboto_300Light,
   });
 
-  useEffect(() => {
-    if (fontsLoaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) return null;
+  if (!fontsLoaded) return null
 
   return (
     <SafeAreaProvider>
       <View style={styles.fullscreen}>
-        <Stack initialRouteName="(auth)/login" screenOptions={{ headerShown: false }} />
+        <Stack screenOptions={{ headerShown: false }} />
       </View>
     </SafeAreaProvider>
   );
@@ -35,6 +25,6 @@ const styles = StyleSheet.create({
   fullscreen: {
     flex: 1,
     backgroundColor: '#f0f0f0',
-    margin: 0
+    margin: 0,
   },
 });
